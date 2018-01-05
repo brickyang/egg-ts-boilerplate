17de0fa6fe68ae67b385c55904b5f3e106e47279
brick <brick.c.yang@gmail.com>
2018-01-05 12:42:07 +0800

    fix: typo in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,12 @@
 import DefaultConfig from './config/config.default';
-import { redis } from './config/plugin';
+import { mongo } from './config/plugin';
 import HomeController from './app/controller/home';
 import HomeService from './app/service/home';
 
 declare module 'egg' {
   export interface Application {
     config: EggAppConfig & DefaulConfig;
+    mongo: mongo;
     bar: string;
   }
 
