fd3a76529a4288ef5ab90db22f5fb98989fa4769
Silver·湫澲 <autumnsilverleaves@gmail.com>
2019-03-13 09:26:56 +0800

    字符串由双引号改为单引号

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -10,8 +10,8 @@ export default class HomeController extends Controller {
 
   public compressed() {
     let data: any[] = new Array(128);
-    data.fill("I love Egg.js and TypeScript.");
-    this.ctx.body = data.join("");
+    data.fill('I love Egg.js and TypeScript.');
+    this.ctx.body = data.join('');
     this.ctx.status = 200;
   }
 }
