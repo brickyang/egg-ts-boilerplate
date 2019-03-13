a9b10b88d29dd693804d01bc683e3e243022907a
Silver·湫澲 <autumnsilverleaves@gmail.com>
2019-03-13 09:33:03 +0800

    字符串由双引号改为单引号

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,11 +1,11 @@
 "use strict";
-const mm = require("egg-mock");
+const mm = require('egg-mock');
 
-describe("test/app/controller/home.test.js", () => {
+describe('test/app/controller/home.test.js', () => {
   let app;
   let ctx;
   before(async () => {
-    mm.consoleLevel("NONE");
+    mm.consoleLevel('NONE');
     app = mm.app();
     await app.ready();
     ctx = app.mockContext();
@@ -14,22 +14,22 @@ describe("test/app/controller/home.test.js", () => {
   afterEach(mm.restore);
   after(() => app.close());
 
-  it("should GET /", async () => {
+  it('should GET /', async () => {
     return app
       .httpRequest()
       .get("/")
-      .expect("I love Egg.js and TypeScript.")
+      .expect('I love Egg.js and TypeScript.')
       .expect(200);
   });
 
-  it("should GET /compressed", async () => {
+  it('should GET /compressed', async () => {
     let data = new Array(128);
-    data.fill("I love Egg.js and TypeScript.");
+    data.fill('I love Egg.js and TypeScript.');
 
     return app
       .httpRequest()
-      .get("/compressed")
-      .expect(data.join(""))
+      .get('/compressed')
+      .expect(data.join(''))
       .expect(200);
   });
 });
