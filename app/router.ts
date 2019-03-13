673de319d9b3439f443d94f1167380e22f3024d9
Silver·湫澲 <autumnsilverleaves@gmail.com>
2019-03-13 09:30:04 +0800

    字符串由双引号改为单引号

--- app/router.ts
+++ app/router.ts
@@ -1,7 +1,7 @@
-import { Application } from "egg";
+import { Application } from 'egg';
 
 export default (app: Application) => {
   const { router, controller } = app;
-  router.get("/", controller.home.index);
-  router.get("/compressed", controller.home.compressed);
+  router.get('/', controller.home.index);
+  router.get('/compressed', controller.home.compressed);
 };
